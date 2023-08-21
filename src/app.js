75ee1c745573d20c3117d79d3f964e72d24985a5
gmonlezun <gabriel.monlezun@gmail.com>
2023-08-21 16:26:28 -0300

    [FEATURE] login|register

--- src/app.js
+++ src/app.js
@@ -4,23 +4,41 @@ import { Server } from 'socket.io';
 
 import { __dirname } from './utils.js';
 
-import productRouter from "./router/products.router.js";
-import cartRouter from "./router/cart.router.js";
-import realtimeRouter from './router/realtimeprods.router.js';
-import chatRouter from './router/chat.router.js';
+import productRouter from "./routes/products.routes.js";
+import cartRouter from "./routes/cart.routes.js";
+import realtimeRouter from './routes/realtimeprods.routes.js';
+import chatRouter from './routes/chat.routes.js';
+import loginRouter from './routes/login.routes.js';
+import sessionRouter from './routes/session.routes.js';
+import signupRouter from './routes/signup.routes.js';
 
 import { ProductManager } from "./dao/dbManagers/DBproductManager.js";
 
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-import { MessageManager } from './dao/dbManagers/DBmessageManager.js';
+import { MessageManager } from './dao/dbManagers/DBmessageManager.js'; 
+
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI);
+const MONGODB_URL = process.env.MONGODB_URL;
+const DB = process.env.DB_NAME
+const MONGODB_URI = `${MONGODB_URL}${DB}`
+// mongoose.connect(MONGODB_URI);
+const environment = async () => {
+    try {
+      await mongoose.connect(MONGODB_URI);
+      console.log("Base de datos conectada");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  environment();
 
 const productManager = new ProductManager()
 const messageManager = new MessageManager()
@@ -31,22 +49,41 @@ app.use(express.urlencoded({extended:true}));
 // Configurar el directorio estático para archivos públicos
 app.use(express.static(__dirname+"/public"));
 
+app.use(
+    session({
+        store: MongoStore.create({
+            mongoUrl: MONGODB_URL,
+            mongoOptions: {
+                useNewUrlParser: true,
+                useUnifiedtopology: true,
+            },
+            ttl:30,
+        }),
+        secret: "codersecret",
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+
 // Configurar el motor de plantillas Handlebars
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", `${__dirname}/views`);
 
 
-
-app.get("/", async (req, res) => {
-    let products = await productManager.getProducts();
-    res.render("home", {products});
-})
+// app.get("/", async (req, res) => {
+//     let products = await productManager.getProducts();
+//     res.render("home", {products});
+// })
 
 app.use("/realtimeproducts", realtimeRouter);
 app.use("/products", productRouter);
 app.use("/cart", cartRouter);
 app.use("/chat", chatRouter);
+app.use("/", loginRouter);
+app.use("/signup", signupRouter);
+app.use("/api/session/", sessionRouter);
+
 
 const httpServer = app.listen(PORT, (e)=>{
     console.log(`Servidor escuchando en el puerto: ${PORT}`);
